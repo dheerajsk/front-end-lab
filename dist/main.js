ae0de386301af6ef8d863c6fce23fcf23a61aac8
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-03-19 12:22:17 +0530

    webpack mode

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,66 @@
-(()=>{var r={801:r=>{r.exports={sum:function(r,t){return r+t},multiply:function(r,t){return r*t}}}},t={};function o(n){var e=t[n];if(void 0!==e)return e.exports;var u=t[n]={exports:{}};return r[n](u,u.exports,o),u.exports}(()=>{const r=o(801);console.log("Result: "+r.multiply(10,20))})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/calculate.js":
+/*!**************************!*\
+  !*** ./src/calculate.js ***!
+  \**************************/
+/***/ ((module) => {
+
+eval("\r\nmodule.exports = {\r\n\r\n    sum: function (a, b) {\r\n        return a + b;\r\n    },\r\n\r\n    multiply: function (a, b) {\r\n        return a * b;\r\n    }\r\n};\n\n//# sourceURL=webpack://front-end-lab/./src/calculate.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("\r\nconst calculate = __webpack_require__(/*! ./calculate */ \"./src/calculate.js\");\r\n\r\nconsole.log(\"Result: \" + calculate.multiply(10, 20));\n\n//# sourceURL=webpack://front-end-lab/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
